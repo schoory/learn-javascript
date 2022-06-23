74b30ec552b145b3b284aa405d412f3144b99279
Alexander <dmitrievcontact@gmail.com>
2022-06-23 16:54:29 +0300

    Added 8 chapter 4 part

---
+++ 8 глава/8.4/main.js
@@ -0,0 +1,23 @@
+// Задание 1. Добавьте toString в словарь.
+let dictionary = Object.create(null);
+
+Object.defineProperty(dictionary, "toString", {
+  value: function() {
+    return Object.keys(this).join(',')
+  },
+  writable: true, 
+  enumerable: false, 
+  configurable: true
+})
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for(let key in dictionary) {
+  alert(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+alert(dictionary); // "apple,__proto__"
